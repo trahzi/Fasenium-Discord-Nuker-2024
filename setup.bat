24a691aa33c4b9920eb9df63cc2477f10170d8eb
trahzi <koddosijustmadethis@gmail.com>
2024-08-13 13:20:58 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -4,8 +4,8 @@
 echo Installing packages..
 
 
-REM Install discord.py and colorama
-pip install discord.py colorama
+REM Install discord.py and colorama and aiohttp
+pip install discord.py colorama aiohttp
 
 REM Prompt user to run the script
 echo Installation complete. Please run `fasenium_source.py` to start.
