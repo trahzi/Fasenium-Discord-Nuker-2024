8ce02e8e7413c0dfbf7e36a47d931a717c9a3b1c
trahzi <koddosijustmadethis@gmail.com>
2024-08-13 12:59:06 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,21 +1,27 @@
 @echo off
 SETLOCAL
+
 python --version >nul 2>&1
 IF ERRORLEVEL 1 (
     echo Python is not installed.
     pause
     exit /b 1
 )
+
 pip install discord.py colorama >nul 2>&1
 IF ERRORLEVEL 1 (
     echo Failed to install packages.
     pause
     exit /b 1
 )
-python fanium.py
+
+echo Current directory: %CD%
+
+python "%CD%\fanium.py"
 IF ERRORLEVEL 1 (
-    echo Error encountered.
+    echo Error encountered while running fanium.py.
     pause
     exit /b 1
 )
+
 pause
