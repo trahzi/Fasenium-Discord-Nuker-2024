d4398e9a1335e5a5f6d1feb9c6c8fdbb7022e590
trahzi <koddosijustmadethis@gmail.com>
2024-08-13 13:07:59 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,27 +1,13 @@
 @echo off
-SETLOCAL
 
-set "found=0"
 
-for /r C:\ %%f in (Fasenium_source.py) do (
-    set "found=1"
-    set "script_path=%%f"
-    goto :found
-)
+echo Installing packages..
 
-:found
-if %found%==1 (
-    echo Running script: %script_path%
-    python "%script_path%"
-    IF ERRORLEVEL 1 (
-        echo Error encountered while running %script_path%.
-        pause
-        exit /b 1
-    )
-) else (
-    echo Fasenium_source.py not found on this computer.
-    pause
-    exit /b 1
-)
 
+REM Install discord.py and colorama
+pip install discord.py colorama
+
+REM Prompt user to run the script
+echo Installation complete. Please run `fasenium_source.py` to start.
 pause
+
